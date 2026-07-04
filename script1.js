8b26f49777d470d5ffffbdaf7bfd80385bef8b94
THARA HUOKAING <tharahuokaing@gmail.com>
2026-07-04 20:15:13 +0700

    Change wedding audio source from music2.mp3 to boat.mp3

--- script1.js
+++ script1.js
@@ -6,7 +6,7 @@
  * Audio Engine Configuration
  * Uses the local wedding audio asset path
  */
-const weddingAudioSrc = "music2.mp3";
+const weddingAudioSrc = "boat.mp3";
 
 // Create HTMLAudioElement instance
 const weddingAudio = new Audio(weddingAudioSrc);
